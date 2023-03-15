a89dfa60e90d0ec5cd3d38c881f05261a27dfd1c
hardikbbhange06 <118076462+hardikbbhange06@users.noreply.github.com>
2023-03-16 01:10:53 +0530

    first commit

---
+++ src/main/java/com/googlesheetread/googlesheetread/GooglesheetreadApplication.java
@@ -0,0 +1,13 @@
+package com.googlesheetread.googlesheetread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglesheetreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglesheetreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/googlesheetread/googlesheetread/GooglesheetreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.googlesheetread.googlesheetread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglesheetreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
